cc2b4c7909c0f51f785c678ce90943536ea9914e
dimasf <dimasf.14.y@gmail.com>
2026-04-15 14:10:45 +0700

    add confusion matrix

--- src/train.py
+++ src/train.py
@@ -6,10 +6,10 @@
 
 import pandas as pd
 import joblib
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from imblearn.over_sampling import RandomOverSampler
 from config import PREPROCESSED, MODEL_FILE, VECTORIZER_FILE, TEST_DATA_FILE, RANDOM_STATE
 
@@ -33,15 +33,26 @@ def main():
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec  = vectorizer.transform(X_test)
 
+
+    # === CROSS VALIDATION ===
+    cv_scores = cross_val_score(
+        MultinomialNB(alpha=1.0),
+        X_train_vec,
+        y_train,
+        cv=5
+    )
+
+    print("\n=== CROSS VALIDATION ===")
+    print("Skor tiap fold:", cv_scores)
+    print(f"Rata-rata CV: {cv_scores.mean()*100:.2f}%")
+
     # 4. Oversampling
     ros = RandomOverSampler(random_state=RANDOM_STATE)
     X_train_bal, y_train_bal = ros.fit_resample(X_train_vec, y_train)
-    print("Distribusi setelah oversampling:", pd.Series(y_train_bal).value_counts().to_dict())
 
     # 5. Training
     model = MultinomialNB(alpha=1.0)
     model.fit(X_train_bal, y_train_bal)
-    print("Urutan Kelas di Model:", model.classes_)
 
     # 6. Simpan model & data uji
     joblib.dump(model, MODEL_FILE)
@@ -50,11 +61,26 @@ def main():
     df_test = pd.DataFrame({"text_processed": X_test, "Label": y_test})
     df_test.to_csv(TEST_DATA_FILE, index=False)
 
-    print("=== PROSES SELESAI ===")
-    print(f"Model berhasil dilatih dengan {X_train_bal.shape[0]} data (setelah oversampling).")
-    print(f"Akurasi pada data uji: {accuracy_score(y_test, model.predict(X_test_vec))*100:.2f}%")
-    print(f"Data uji disimpan di: {TEST_DATA_FILE}")
+    # Evaluasi
+    y_pred = model.predict(X_test_vec)
 
+    print("\n=== EVALUASI MODEL ===")
+    print("Distribusi setelah oversampling:", pd.Series(y_train_bal).value_counts().to_dict())
+
+    # Accuracy
+    acc = accuracy_score(y_test, y_pred)
+    print(f"Akurasi: {acc*100:.2f}%")
+
+    # Classification Report
+    print("\nClassification Report:")
+    print(classification_report(y_test, y_pred))
+
+    # Confusion Matrix
+    print("\nConfusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+
+    # Hasil oversampling
+    print(f"Model berhasil dilatih dengan {X_train_bal.shape[0]} data (setelah oversampling).")
 
 if __name__ == "__main__":
     main()
