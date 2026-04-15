58bc4ebc475ecebfdc0848b3031dd98dab403a59
dimasf <dimasf.14.y@gmail.com>
2026-04-15 14:17:25 +0700

    fix directory

--- src/config.py
+++ src/config.py
@@ -3,12 +3,13 @@ import os
 # Base directory = project root (d:\Tugas Akhir)
 BASE_DIR = os.path.join(os.path.dirname(__file__), "..")
 DATA_DIR = os.path.join(BASE_DIR, "data")
+MODEL_DIR = os.path.join(BASE_DIR, "model")
 
 # File paths
 INPUT_FILE      = os.path.join(DATA_DIR, "dataset_TA.xlsx")
 PREPROCESSED    = os.path.join(DATA_DIR, "dataset_preprocessed.csv")
-MODEL_FILE      = os.path.join(DATA_DIR, "model_mnb.pkl")
-VECTORIZER_FILE = os.path.join(DATA_DIR, "vectorizer.pkl")
+MODEL_FILE      = os.path.join(MODEL_DIR, "model_mnb.pkl")
+VECTORIZER_FILE = os.path.join(MODEL_DIR, "vectorizer.pkl")
 TEST_DATA_FILE  = os.path.join(DATA_DIR, "data_ujian.csv")
 
 # Shared settings
